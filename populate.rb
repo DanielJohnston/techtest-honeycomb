fd344dcd562d996bf8251c5a6e9178bcedc22365
Daniel Whitston <dan@whitston.org.uk>
2017-05-17 19:40:51 +0100

    post-deadline fix: populate.rb assign broadcasters

--- populate.rb
+++ populate.rb
@@ -12,13 +12,13 @@ discount_list.add Discount.new(ExpressMultipurchaseDiscount.new(express))
 discount_list.add Discount.new(Over30Discount.new)
 
 broadcasters = {}
-broadcasters['Viacom'] = Broadcaster.new 'Viacom'
-broadcasters['Disney'] = Broadcaster.new 'Disney'
-broadcasters['Discovery'] = Broadcaster.new 'Discovery'
-broadcasters['ITV'] = Broadcaster.new 'ITV'
-broadcasters['Channel 4'] = Broadcaster.new 'Channel 4'
-broadcasters['Bike Channel'] = Broadcaster.new 'Bike Channel'
-broadcasters['Horse and Country'] = Broadcaster.new 'Horse and Country'
+broadcasters[:viacom] = Broadcaster.new 'Viacom'
+broadcasters[:disney] = Broadcaster.new 'Disney'
+broadcasters[:discovery] = Broadcaster.new 'Discovery'
+broadcasters[:itv] = Broadcaster.new 'ITV'
+broadcasters[:channel_4] = Broadcaster.new 'Channel 4'
+broadcasters[:bike_channel] = Broadcaster.new 'Bike Channel'
+broadcasters[:horse_and_country] = Broadcaster.new 'Horse and Country'
 
 order_1 = Order.new Material.new('WNP/SWCL001/010'), discount_list, DeliveryList.new
 order_1.add_delivery(Delivery.new(broadcasters[:disney], standard))
